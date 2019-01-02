c9f72f6229c43c248ecc88efa12c71d036fe3924
Paweł <pawellwankiewicz@gmail.com>
2019-01-02 21:10:17 +0100

    created service and nodejs api with customers

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,17 +1,28 @@
 (function() {
 
-    const CustomersController = function($scope) {
+    const CustomersController = function($scope, $log, customersFactory, appSettings) {
         $scope.sortBy = 'name';
         $scope.reverse = false;
+        $scope.customers = [];
+        $scope.appSettings = appSettings;
 
-        $scope.customers = [{ joined: '2000-12-02', name: 'John', city: 'Chandler', orderTotal: 9.9956 }, { joined: '1965-01-25', name: 'Zed', city: 'Las Vegas', orderTotal: 19.99 }, { joined: '1944-06-15', name: 'Tina', city: 'New York', orderTotal: 44.99 }, { joined: '1995-03-28', name: 'Dave', city: 'Seattle', orderTotal: 101.50 }];
+        function init() {
+            customersFactory.getCustomers()
+                .success(function(customers) {
+                    $scope.customers = customers;
+                })
+                .error(function(data, status, header, config) {
+                    $log.log(data.error + ' ' + status);
+                });
+        }
+        init();
         $scope.doSort = function(propName) {
             $scope.sortBy = propName;
             $scope.reverse = !$scope.reverse;
         };
     };
 
-    CustomersController.$inject = ['$scope'];
+    CustomersController.$inject = ['$scope', '$log', 'customersFactory', 'appSettings'];
 
     angular.module('customersApp')
         .controller('CustomersController', CustomersController);
---
+++ app/controllers/ordersController.js
@@ -0,0 +1,25 @@
+(function() {
+
+    var OrdersController = function($scope, $log, $routeParams, customersFactory) {
+        var customerId = $routeParams.customerId;
+        $scope.customer = null;
+
+        function init() {
+            customersFactory.getCustomer(customerId)
+                .success(function(customer) {
+                    $scope.customer = customer;
+                })
+                .error(function(data, status, header, config) {
+                    $log.log(data.error + ' ' + status);
+                });
+        }
+
+        init();
+    };
+
+    OrdersController.$inject = ['$scope', '$log', '$routeParams', 'customersFactory'];
+
+    angular.module('customersApp')
+        .controller('OrdersController', OrdersController);
+
+}());
