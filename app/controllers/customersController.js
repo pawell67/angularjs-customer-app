f6830b8992c706359c1bcee62da9e3c06b1361a4
Paweł <pawellwankiewicz@gmail.com>
2019-01-02 21:53:55 +0100

    added all orders view and bootstrap

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,30 +1,53 @@
 (function() {
-
-    const CustomersController = function($scope, $log, customersFactory, appSettings) {
+  
+    var CustomersController = function ($scope, $log, $window, customersFactory, appSettings) {
         $scope.sortBy = 'name';
         $scope.reverse = false;
         $scope.customers = [];
         $scope.appSettings = appSettings;
-
+        
         function init() {
             customersFactory.getCustomers()
-                .success(function(customers) {
-                    $scope.customers = customers;
-                })
-                .error(function(data, status, header, config) {
+                .then(function(response) {
+                    $scope.customers = response.data;
+                }, function(data, status, headers, config) {
                     $log.log(data.error + ' ' + status);
                 });
         }
+        
         init();
+        
         $scope.doSort = function(propName) {
-            $scope.sortBy = propName;
-            $scope.reverse = !$scope.reverse;
+           $scope.sortBy = propName;
+           $scope.reverse = !$scope.reverse;
+        };
+        
+        $scope.deleteCustomer = function(customerId) {
+            customersFactory.deleteCustomer(customerId)
+                .then(function(response) {
+                    var status = response.data;
+                    if (status) {
+                        for (var i=0,len=$scope.customers.length;i<len;i++) {
+                            if ($scope.customers[i].id === customerId) {
+                               $scope.customers.splice(i,1);
+                               break;
+                            }
+                        }  
+                    }
+                    else {
+                        $window.alert('Unable to delete customer');   
+                    }
+                    
+                }, function(data, status, headers, config) {
+                    $log.log(data.error + ' ' + status);
+                });
         };
     };
-
-    CustomersController.$inject = ['$scope', '$log', 'customersFactory', 'appSettings'];
+    
+    CustomersController.$inject = ['$scope', '$log', '$window', 'customersFactory', 
+                                   'appSettings'];
 
     angular.module('customersApp')
-        .controller('CustomersController', CustomersController);
-
+      .controller('CustomersController', CustomersController);
+    
 }());
